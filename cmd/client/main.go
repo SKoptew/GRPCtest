9f755e3dfde7a630fdf9ae2135bb09bb92e0fa5e
Sergey Koptev <skoptew@gmail.com>
2021-11-18 01:42:04 +0300

    Workable tiny server and clients

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"grpctest/pkg/api"
+	"log"
+	"time"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("cannot connect:", err)
+	}
+	defer conn.Close()
+
+	client := api.NewAdderClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	response, err := client.Add(ctx, &api.AddRequest{X: 1, Y: 4})
+	if err != nil {
+		log.Fatal("cannot remote call Add", err)
+	}
+	log.Print("result:", response.GetResult())
+}
